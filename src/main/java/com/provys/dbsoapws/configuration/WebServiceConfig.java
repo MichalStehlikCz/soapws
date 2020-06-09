5de9d0ffffcb01178115d3700961f17e354d3333
MichalStehlikCz <michal.stehlik.cz@gmail.com>
2020-06-09 13:50:15 +0200

    MessageDispatcherServlet only listens on paths, defined in services

--- src/main/java/com/provys/dbsoapws/configuration/WebServiceConfig.java
+++ src/main/java/com/provys/dbsoapws/configuration/WebServiceConfig.java
@@ -1,6 +1,10 @@
 package com.provys.dbsoapws.configuration;
 
+import com.provys.common.exception.InternalException;
+import com.provys.dbsoapws.model.EndpointDefinition;
 import com.provys.dbsoapws.model.ServiceDefinition;
+import java.util.Collection;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
@@ -13,11 +17,21 @@ import org.springframework.ws.transport.http.MessageDispatcherServlet;
 @Configuration
 public class WebServiceConfig extends WsConfigurerAdapter {
 
+  private static void validateEndpoints(Collection<? extends EndpointDefinition> endpoints) {
+    endpoints.stream()
+        .filter(endpoint -> endpoint.getPath().equals("/") || endpoint.getPath().equals("/error"))
+        .findFirst()
+        .ifPresent(endpoint -> {
+          throw new InternalException(
+              "Endpoint " + endpoint.getName() + " has invalid path " + endpoint.getPath());
+        });
+  }
+
   /**
    * Registers our special message dispatcher servlet.
    *
    * @param applicationContext is Spring application context
-   * @param serviceDefinition is service definition, loaded from database
+   * @param serviceDefinition  is service definition, loaded from database
    * @return registration bean for our message dispatcher servlet
    */
   @Bean
@@ -27,7 +41,13 @@ public class WebServiceConfig extends WsConfigurerAdapter {
     MessageDispatcherServlet servlet = new DbMessageDispatcherServlet(serviceDefinition);
     servlet.setApplicationContext(applicationContext);
     servlet.setTransformWsdlLocations(true);
-    return new ServletRegistrationBean<>(servlet, "/*");
+    validateEndpoints(serviceDefinition.getEndpoints());
+    //noinspection ZeroLengthArrayAllocation
+    var urlMappings = serviceDefinition.getEndpoints().stream()
+        .map(EndpointDefinition::getPath)
+        .collect(Collectors.toUnmodifiableList())
+        .toArray(new String[0]);
+    return new ServletRegistrationBean<>(servlet, urlMappings);
   }
 
   /**
